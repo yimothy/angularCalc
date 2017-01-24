2278be1c46f061c90fc3b61741999663e2eb0276
Timothy Yin <tyin92@gmail.com>
2017-01-24 18:57:00 -0500

    Finished basic calculator displaying only the result

--- calc.js
+++ calc.js
@@ -3,6 +3,8 @@ const module = angular.module('calc', [])
 .controller('CalcController', function($scope) {
   $scope.storage = {};
   $scope.storage.num = '';
+  $scope.storage.num1 = '';
+  $scope.storage.num2 = '';
   $scope.storage.result;
   $scope.storage.store = [];
   $scope.storage.selectedOp = null;
@@ -25,14 +27,15 @@ const module = angular.module('calc', [])
     console.log('button: ', button);
     if(button !== '+' && button !== '-' && button !== 'x' && button !== '/') {
       $scope.storage.num += button;
-      console.log('num: ', $scope.storage.num);
+      // console.log('num: ', $scope.storage.num);
     }
     else if ($scope.storage.store.length === 0 && $scope.storage.num !== ''){
+      $scope.storage.num1 = '';
       $scope.storage.store.push($scope.storage.num, button)
       $scope.storage.num = '';
     }
     // $scope.storage.push(button);
-    console.log("storage: ", $scope.storage.store)
+    // console.log("storage: ", $scope.storage.store)
   }
 
   $scope.calc = function() {
