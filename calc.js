78ff65f04a44553ee4e8bd70068f234040a29ec7
Timothy Yin <tyin92@gmail.com>
2017-01-24 18:43:13 -0500

    Created controller functions

--- calc.js
+++ calc.js
@@ -1,8 +1,43 @@
 const module = angular.module('calc', [])
 
 .controller('CalcController', function($scope) {
-  $scope.test = function(){
-    console.log('it worked!');
+  $scope.storage = {};
+  $scope.storage.num = '';
+  $scope.storage.result;
+  $scope.storage.store = [];
+  $scope.storage.selectedOp = null;
+  $scope.storage.operators = {
+    '+': function(a, b) {
+      return a + b;
+    },
+    '-': function(a, b) {
+      return a - b;
+    },
+    "x": function(a, b) {
+      return a*b;
+    },
+    "/": function(a, b) {
+      return a / b;
+    },
+  }
+
+  $scope.store = function(button){
+    console.log('button: ', button);
+    if(button !== '+' && button !== '-' && button !== 'x' && button !== '/') {
+      $scope.storage.num += button;
+      console.log('num: ', $scope.storage.num);
+    }
+    else if ($scope.storage.store.length === 0 && $scope.storage.num !== ''){
+      $scope.storage.store.push($scope.storage.num, button)
+    }
+    // $scope.storage.push(button);
+    console.log("storage: ", $scope.storage.store)
+  }
+
+  $scope.calc = function() {
+    $scope.storage.store.push($scope.storage.num);
+    $scope.storage.result = $scope.storage.operators[$scope.storage.store[1]]($scope.storage.store[0], $scope.storage.store[2]);
+    console.log('result: ')
   }
 
 })
